0dc9ef77fe80088b12917d865f06c61afc349657
Yaromir_Svetozarovich <svetozar.derendery@yandex.ru>
2021-02-15 14:08:04 +0700

    add IMGAE_SIZER(for wide putin, tu-tutu ;) )

--- numbers_on_PNG.py
+++ numbers_on_PNG.py
@@ -33,16 +33,35 @@ def image_mod(input_path,output_path,width_new, height_new,size, x, y,):
     pix = image.load()
     draw = ImageDraw.Draw(image_new)
     font = ImageFont.truetype("1298.ttf", size)
-    step = round(size*pi/1.7)
+    step = round(size*pi/1.75)
     for i in range(0,width,step):
-        for j in range(0,height,round(step/2)):
+        for j in range(0,height,step):
             r = pix[i, j][0]
             g = pix[i, j][1]
             b = pix[i, j][2]
-            draw.text((i,j), (num_generator(4,9)), font = font, fill = (r,g,b))
+            draw.text((i,j//3), (num_generator(4,9)), font = font, fill = (r,g,b))
+    image_new.save(output_path)
+    image_new.show()
+
+def image_sizer(input_path,output_path,width_new, height_new,size, x, y,):
+    image = Image.open(input_path)
+    image_new = Image.new("RGB", (width_new,height_new), "black")
+    width = image.size[0]
+    height = image.size[1]
+    pix = image.load()
+    draw = ImageDraw.Draw(image_new)
+    step = 1
+    bias = image.size 
+    for i in range(0,width,step):
+        for j in range(0,height,step):
+            r = pix[i, j][0]
+            g = pix[i, j][1]
+            b = pix[i, j][2]
+            draw.line((i,j)+bias, fill = (r,g,b))
     image_new.save(output_path)
     image_new.show()
 
 if __name__ =="__main__":
     #text("text.png",width = 2160, height = 3840, x = 1, y = 1, n = 180, size = 8)
-    image_mod("input1.png", "output.png", 2160,3840, 8, 0, 0)
+    #image_mod("putin.png", "output_5.png",3840 ,2160, 4, 0, 0)
+    #image_sizer("input1.png", "output_3.png",3840 ,2160, 4, 0, 0)
